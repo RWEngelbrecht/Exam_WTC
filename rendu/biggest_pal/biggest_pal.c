22f222c27f4e427adc5cc312f4eede397e028162
RWEngelbrecht <45070199+RWEngelbrecht@users.noreply.github.com>
2019-08-20 09:21:06 +0200

    test in examshell to see if it passes

--- rendu/biggest_pal/biggest_pal.c
+++ rendu/biggest_pal/biggest_pal.c
@@ -1,74 +1,58 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   biggest_pal.c                                      :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: rengelbr <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/08/19 09:54:02 by rengelbr          #+#    #+#             */
-/*   Updated: 2019/08/19 10:48:52 by rengelbr         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <unistd.h>
 
-void	print_pal(char *str, int start, int end)
+void print_pal(char *s, int start, int end)
 {
 	while (start <= end)
-	{
-		write(1, &str[start++], 1);
-	}
+		write(1, &s[start++], 1);
 }
 
-void	biggest_pal(char *str)
+void biggest_pal(char *s)
 {
 	int i;
 	int len;
+	int big_len;
 	int start;
 	int end;
-	int biggest_len;
 	int hold;
 
-
 	len = 0;
-	while (str[len])
+	i = 0;
+	while (s[len])
 		len++;
-	if (len == 1)				//SINGLE LETTERS ARE PALINDROMES!!!
-		write(1, str, 1);
-	i = 1;
-	biggest_len = 1;
+	if (len == 1)
+		write(1, &s[i], 1);
+	big_len = 1;
 	while (++i < len)
 	{
 		start = i - 1;
 		end = i;
-		while (start >= 0 && end < len && str[start] == str[end])
+		while (start >= 0 && end < len && s[start] == s[end])
 		{
-			if (end - start + 1 >= biggest_len)
+			if (end - start + 1 >= big_len)
 			{
 				hold = start;
-				biggest_len = end - start + 1;
+				big_len = end - start + 1;
 			}
 			--start;
 			++end;
 		}
 		start = i - 1;
 		end = i + 1;
-		while (start >= 0 && end < len && str[start] == str[end])
+		while (start >= 0 && end < len && s[start] == s[end])
 		{
-			if (end - start + 1 >= biggest_len)
+			if (end - start + 1 >= big_len)
 			{
 				hold = start;
-				biggest_len = end - start + 1;
+				big_len = end - start + 1;
 			}
 			--start;
 			++end;
 		}
 	}
-	print_pal(str, hold, hold + biggest_len - 1);
-
+	print_pal(s, hold, hold + big_len - 1);
 }
 
-int		main(int ac, char **av)
+int main(int ac, char **av)
 {
 	if (ac == 2)
 	{
